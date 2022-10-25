2e819e84fd2e10c04376edbaf8d0f35be98c89ce
unknown <ajaylomate15@gmail.com>
2022-10-25 18:56:22 +0530

    test file added

---
+++ test.py
@@ -0,0 +1,17 @@
+# import os
+
+# import pexpect
+# import config
+# import pickle
+# import json
+
+#  with open(config.DATA_PATH,'r') as f:
+#          bike_data = json.load(f)
+
+#  print(bike_data)
+
+# with open(config.MODEL_PATH,'rb') as f:
+#         bike_model = pickle.load(f)
+
+
+# print(bike_model)
